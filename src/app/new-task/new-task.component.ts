9556d78578eaad811777bd576f4d9a2c8371c6dc
velezmidi <velez.midi@gmail.com>
2021-05-26 23:35:51 -0500

    eee

--- src/app/new-task/new-task.component.ts
+++ src/app/new-task/new-task.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewTaskComponent implements OnInit {
 
+  getVal(val:any){
+    console.log(val)
+  }
+
   constructor() { }
 
   ngOnInit(): void {
